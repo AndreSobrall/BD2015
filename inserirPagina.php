13129dda2ed0c53966b0840ddc0f1bb4ae70d9b1
Andre Sobral <andre.sobral@tecnico.ulisboa.pt>
2016-07-26 00:50:26 +0100

    Menu inicial terminado

---
+++ inserirPagina.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>	
+		<link rel="stylesheet" href="styles.css">
+		<meta charset="UTF-8">
+		<title> BD 2015/2016 </title> 
+</head>
+<body>
+	<h1> Inserir uma nova página </h1>
+	<h2> Campos </h2>
+	<form>
+  	Nome da Página: <input type="text" name="firstname"><br>
+	</form>
+</body>
+</html>
+
