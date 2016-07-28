163aea161dea0625c012cd3f5991c9bb8250d015
Andre <Andre@Andres-iMac.lan>
2016-07-28 14:41:03 +0100

    login html

---
+++ Login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+		<link rel="stylesheet" href="styles.css">
+		<meta charset="UTF-8">
+		<title> BD 2015/2016 </title> 
+</head>
+<body>
+	<div class="div">
+			<h1> Bases de Dados 2015/2016 </h1>
+			<h2> Login </h2>
+			<form>
+				Username: <input type="text" name="username"><br>
+				Password :  <input type="text" name="password"><br><br><br>
+			</form>
+			<form action="http://localhost:8888/Registar.php">
+				<button type="submit"> Registar </button>
+			</form>
+	</div>
+</body>
+</html>
